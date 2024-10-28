bce955289c57777c10d834b911c02d8b73ef8390
ravinbraon <127830878+ravinbraon@users.noreply.github.com>
2024-10-28 10:24:05 +0100

    wip

---
+++ src/script.js
@@ -0,0 +1,27 @@
+const doc = document;
+const myButton = doc.getElementById("btn");
+const myParagraph = doc.getElementById("txtJoke");
+
+
+
+myButton.addEventListener("click", function () {
+    function randomJoke() {
+        return fetch('http://localhost:3000/blagues/random')
+        .then((httpResponse) => {
+            console.log('fetch effectué');
+            if (!httpResponse.ok) {
+                throw new Error("Erreur lors de la récupération de la blague");
+            }
+            return httpResponse.json();
+        });
+    }
+
+    randomJoke()
+    .then((data) => {
+        myParagraph.textContent = data.joke
+    })
+    .catch((error) => {
+        console.error("Erreur :", error);
+        myParagraph.textContent = "Erreur lors de la récupération de la blague.";
+    });
+})
