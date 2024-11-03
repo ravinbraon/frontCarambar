e85e283a46a947e812f9e69cb162d98944d3d9b8
ravinbraon <127830878+ravinbraon@users.noreply.github.com>
2024-11-03 17:37:45 +0100

    wip

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ const doc = document;
 const myButton = doc.getElementById("btn");
 const myParagraph = doc.getElementById("txtJoke");
 
-
-
 myButton.addEventListener("click", function () {
+    this.classList.add("zoom");
+    myParagraph.textContent = "Chargement de la blague...";
+
+    setTimeout(() => {
+        this.classList.remove("zoom");
+    }, 300);
     function randomJoke() {
         return fetch('https://apicarambar-vcrp.onrender.com/blagues/random/')
         .then((httpResponse) => {
